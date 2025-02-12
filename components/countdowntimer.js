dbd1db5c05c1b8a1742078fb6731f7e0d74d4d22
Eirikfin <148289492+Eirikfin@users.noreply.github.com>
2025-02-12 14:42:42 +0100

    countdown

--- components/countdowntimer.js
+++ components/countdowntimer.js
@@ -3,8 +3,10 @@ export default class CountdownTimer extends HTMLElement {
       super();
       this.attachShadow({ mode: "open" });
   
+      // properties:
       this.clocktimer = ['ready', 'set', 'GO!'];
       this.time = 0;
+      this.countDown = this.getAttribute('seconds') || 5;
   
      
 
@@ -16,38 +18,52 @@ export default class CountdownTimer extends HTMLElement {
     this.timeDisplay = this.shadowRoot.querySelector('#clock');
     this.startBtn = this.shadowRoot.querySelector('#start-button');
     this.resetBtn = this.shadowRoot.querySelector('#reset-button');
+    this.countDownMsg = this.shadowRoot.querySelector('#countdown-msg')
 
     // adding eventlistener:
-    this.startBtn.addEventListener("click", () =>{this.startTimer()});
+    this.startBtn.addEventListener("click", () =>{this.startRace()});
     this.resetBtn.addEventListener("click", () =>{this.resetTimer()});
     }
-  
+    // methods:
+    startRace() {
+        let countdownIndex = 0;
+    
+        // Show "ready, set, GO!" messages before starting the timer
+        const countdownInterval = setInterval(() => {
+          if (countdownIndex < this.clocktimer.length) {
+            this.countDownMsg.textContent = this.clocktimer[countdownIndex];
+            countdownIndex++;
+          } else {
+            clearInterval(countdownInterval);
+            this.countDownMsg.textContent = ""; // Clear message after GO!
+            this.startTimer(); // Start the main timer
+          }
+        }, 1000);
+      }
     startTimer() {
        
         this.runningTime = setInterval(() => {
     
         this.time += 0.01; 
-        this.timeDisplay.textContent = `time: ${this.time.toFixed(2)}`;
+        this.timeDisplay.textContent = `${this.time.toFixed(2)}`;
       }, 10);
-
+      this.startBtn.disabled = true;
     
     }
     resetTimer(){
         clearInterval(this.runningTime)
         this.time= 0;
-        this.timeDisplay.textContent = `time: ${this.time.toFixed(2)}`;
-
+        this.timeDisplay.textContent = ` ${this.time.toFixed(2)}`;
+        this.startBtn.disabled = false;
     }
     render() {
-     // elements in shadowDOM:
     
-
-
-
+    
       this.shadowRoot.innerHTML = `
         <div>
           <h1>Countdown Timer:</h1>
           <p id="timer">time: <span id="clock">${this.time.toFixed(2)}</span></p>
+          <p id="countdown-msg"></p>
             
           <button id="start-button">Start</button>
           <button id="reset-button">Reset</button>
